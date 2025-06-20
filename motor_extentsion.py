7112cd18909a469684ab0dd6893fdd8847a10a09
CSX Operator <xf23id1@xf23id1-ws2.nsls2.bnl.local>
2025-06-20 17:19:58 -0400

    Fix Pv connection

--- motor_extentsion.py
+++ motor_extentsion.py
@@ -9,40 +9,47 @@ from bluesky.plan_stubs import mv
 from bluesky.preprocessors import SupplementalData
 
 class LookupPair(Device):
-    pair_name = FCpt(EpicsSignal, "{self.name_postfix}", kind="config")
-    pair_val = FCpt(EpicsSignal, "{self.val_postfix}", kind="config")
+    pair_name = FCpt(EpicsSignal, "{self.prefix}{self.name_postfix}", kind="config")
+    pair_val = FCpt(EpicsSignal, "{self.prefix}{self.val_postfix}", kind="config")
+
+    
     
     def __init__(self, *args, name_postfix : str, val_postfix : str, **kwargs):
         self.name_postfix = name_postfix
         self.val_postfix = val_postfix
-        super().__init__(**kwargs)
+        super().__init__(*args, **kwargs)
+        
 
 
 from collections import OrderedDict
 
-defn = OrderedDict({
-    "pair1": (LookupPair, "", {"name_postfix" : "Pos-Sel.ONST", "val_postfix" : "Val:1-SP"}),
-    "pair2": (LookupPair, "", {"name_postfix" : "Pos-Sel.TWST", "val_postfix" : "Val:2-SP"}),
-    "pair3": (LookupPair, "", {"name_postfix" : "Pos-Sel.THST", "val_postfix" : "Val:3-SP"}),
-    "pair4": (LookupPair, "", {"name_postfix" : "Pos-Sel.FOST", "val_postfix" : "Val:4-SP"}),
-    "pair5": (LookupPair, "", {"name_postfix" : "Pos-Sel.FIST", "val_postfix" : "Val:5-SP"}),
-    "pair6": (LookupPair, "", {"name_postfix" : "Pos-Sel.SIST", "val_postfix" : "Val:6-SP"}),
-    "pair7": (LookupPair, "", {"name_postfix" : "Pos-Sel.SEST", "val_postfix" : "Val:7-SP"}),
-    "pair8": (LookupPair, "", {"name_postfix" : "Pos-Sel.EIST", "val_postfix" : "Val:8-SP"}),
-    "pair9": (LookupPair, "", {"name_postfix" : "Pos-Sel.NIST", "val_postfix" : "Val:9-SP"}),
-    "pair10": (LookupPair, "", {"name_postfix" : "Pos-Sel.TEST", "val_postfix" : "Val:10-SP"})
-})
 
 
 class DynamicLookup(Device):
+    defn = OrderedDict({
+        "pair1": (LookupPair, "", {"name_postfix" : "Pos-Sel.ONST", "val_postfix" : "Val:1-SP"}),
+        # "pair2": (LookupPair, "", {"name_postfix" : "Pos-Sel.TWST", "val_postfix" : "Val:2-SP"}),
+        # "pair3": (LookupPair, "", {"name_postfix" : "Pos-Sel.THST", "val_postfix" : "Val:3-SP"}),
+        # "pair4": (LookupPair, "", {"name_postfix" : "Pos-Sel.FOST", "val_postfix" : "Val:4-SP"}),
+        # "pair5": (LookupPair, "", {"name_postfix" : "Pos-Sel.FIST", "val_postfix" : "Val:5-SP"}),
+        # "pair6": (LookupPair, "", {"name_postfix" : "Pos-Sel.SIST", "val_postfix" : "Val:6-SP"}),
+        # "pair7": (LookupPair, "", {"name_postfix" : "Pos-Sel.SEST", "val_postfix" : "Val:7-SP"}),
+        # "pair8": (LookupPair, "", {"name_postfix" : "Pos-Sel.EIST", "val_postfix" : "Val:8-SP"}),
+        # "pair9": (LookupPair, "", {"name_postfix" : "Pos-Sel.NIST", "val_postfix" : "Val:9-SP"}),
+        # "pair10": (LookupPair, "", {"name_postfix" : "Pos-Sel.TEST", "val_postfix" : "Val:10-SP"})
+    })
+
     lookupPairs = DynamicDeviceComponent(defn)
 
+        
 
 class CustomMotor(EpicsMotor):
     pos_lookup = Cpt(DynamicLookup, "")
 
-    def __init__(self, name, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, name = "", prefix="", **kwargs):
+        super().__init__(prefix, name = name, **kwargs)
+        self.name = name
+        self.prefix = prefix
 
 
     def lookup_by_name(self, name: str) -> float:
