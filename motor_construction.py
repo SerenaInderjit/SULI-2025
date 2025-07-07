e3e00bf8d1c71baf2508d154aa028780fec4f1c0
SerenaInderjit <sinderjit@bnl.gov>
2025-07-07 09:33:21 -0400

    Fixed string in baseline

--- motor_construction.py
+++ motor_construction.py
@@ -43,7 +43,7 @@ class MotorWithLookup(Device):
 
     pos_lookup = DynamicDeviceComponent(defn)
     motor = Cpt(EpicsMotor, "Mtr")
-    pos_sel = Cpt(EpicsSignal, "Pos-Sel")
+    pos_sel = Cpt(EpicsSignal, "Pos-Sel", string=True)
     
 
     def __init__(self, prefix : str, *args, name = "", **kwargs):
@@ -64,7 +64,8 @@ class MotorWithLookup(Device):
         print(f"{length} possible positions:")
         print("----------------------------------")
         for pair in pair_lst:
-            print(f'    {pair.pair_name:_<15} : {pair.pair_val}')
+            if pair.pair_name != "Undefined":
+                print(f'    {pair.pair_name:_<15} : {pair.pair_val}')
 
 
     def set_pos(self, pos: str | float):
@@ -98,16 +99,29 @@ class MotorWithLookup(Device):
 class SlitsWithLookup(Device):
 
     defn_x = OrderedDict({
-        "pair1": (LookupPair, "", {"name_postfix" : "-Ax:X}Pos-Sel.ONST", "val_postfix" : "-Ax:X}Val:1-SP"}),
-        "pair2": (LookupPair, "", {"name_postfix" : "-Ax:X}Pos-Sel.TWST", "val_postfix" : "-Ax:X}Val:2-SP"}),
-        "pair3": (LookupPair, "", {"name_postfix" : "-Ax:X}Pos-Sel.THST", "val_postfix" : "-Ax:X}Val:3-SP"}),
-        "pair4": (LookupPair, "", {"name_postfix" : "-Ax:X}Pos-Sel.FRST", "val_postfix" : "-Ax:X}Val:4-SP"})
+        "pair1": (LookupPair, "", {"name_postfix" : "-MC10}Pos-Sel.ONST", "val_postfix" : "-MC10}Val:1-SP"}),
+        "pair2": (LookupPair, "", {"name_postfix" : "-MC10}Pos-Sel.TWST", "val_postfix" : "-MC10}Val:2-SP"}),
+        "pair3": (LookupPair, "", {"name_postfix" : "-MC10}Pos-Sel.THST", "val_postfix" : "-MC10}Val:3-SP"}),
+        "pair4": (LookupPair, "", {"name_postfix" : "-MC10}Pos-Sel.FRST", "val_postfix" : "-MC10}Val:4-SP"}), 
+        "pair5": (LookupPair, "", {"name_postfix" : "-MC10}Pos-Sel.FVST", "val_postfix" : "-MC10}Val:5-SP"}), 
+        "pair6": (LookupPair, "", {"name_postfix" : "-MC10}Pos-Sel.SXST", "val_postfix" : "-MC10}Val:6-SP"}), 
+        "pair7": (LookupPair, "", {"name_postfix" : "-MC10}Pos-Sel.SVST", "val_postfix" : "-MC10}Val:7-SP"}), 
+        "pair8": (LookupPair, "", {"name_postfix" : "-MC10}Pos-Sel.EIST", "val_postfix" : "-MC10}Val:8-SP"}), 
+        "pair9": (LookupPair, "", {"name_postfix" : "-MC10}Pos-Sel.NIST", "val_postfix" : "-MC10}Val:9-SP"}), 
+        "pair10": (LookupPair, "", {"name_postfix" : "-MC10}Pos-Sel.TEST", "val_postfix" : "-MC10}Val:10-SP"})
+
     })
     defn_y = OrderedDict({
-        "pair1": (LookupPair, "", {"name_postfix" : "-Ax:Y}Pos-Sel.ONST", "val_postfix" : "-Ax:Y}Val:1-SP"}),
-        "pair2": (LookupPair, "", {"name_postfix" : "-Ax:Y}Pos-Sel.TWST", "val_postfix" : "-Ax:Y}Val:2-SP"}),
-        "pair3": (LookupPair, "", {"name_postfix" : "-Ax:Y}Pos-Sel.THST", "val_postfix" : "-Ax:Y}Val:3-SP"}),
-        "pair4": (LookupPair, "", {"name_postfix" : "-Ax:Y}Pos-Sel.FRST", "val_postfix" : "-Ax:Y}Val:4-SP"})
+        "pair1": (LookupPair, "", {"name_postfix" : "-MC10}Pos-Sel.ONST", "val_postfix" : "-Ax:Y}Val:1-SP"}),
+        "pair2": (LookupPair, "", {"name_postfix" : "-MC10}Pos-Sel.TWST", "val_postfix" : "-Ax:Y}Val:2-SP"}),
+        "pair3": (LookupPair, "", {"name_postfix" : "-MC10}Pos-Sel.THST", "val_postfix" : "-Ax:Y}Val:3-SP"}),
+        "pair4": (LookupPair, "", {"name_postfix" : "-MC10}Pos-Sel.FRST", "val_postfix" : "-Ax:Y}Val:4-SP"}),
+        "pair5": (LookupPair, "", {"name_postfix" : "-MC10}Pos-Sel.FVST", "val_postfix" : "-Ax:Y}Val:5-SP"}), 
+        "pair6": (LookupPair, "", {"name_postfix" : "-MC10}Pos-Sel.SXST", "val_postfix" : "-Ax:Y}Val:6-SP"}), 
+        "pair7": (LookupPair, "", {"name_postfix" : "-MC10}Pos-Sel.SVST", "val_postfix" : "-Ax:Y}Val:7-SP"}), 
+        "pair8": (LookupPair, "", {"name_postfix" : "-MC10}Pos-Sel.EIST", "val_postfix" : "-Ax:Y}Val:8-SP"}), 
+        "pair9": (LookupPair, "", {"name_postfix" : "-MC10}Pos-Sel.NIST", "val_postfix" : "-Ax:Y}Val:9-SP"}), 
+        "pair10": (LookupPair, "", {"name_postfix" : "-MC10}Pos-Sel.TEST", "val_postfix" : "-Ax:Y}Val:10-SP"})
     })
 
 
@@ -115,7 +129,7 @@ class SlitsWithLookup(Device):
     pos_lookup_y = DynamicDeviceComponent(defn_y)
     x = Cpt(EpicsMotor, '-Ax:X}Mtr', name='x')
     y = Cpt(EpicsMotor, '-Ax:Y}Mtr', name='y')
-    pos_sel = Cpt(EpicsSignal, "}Pos-Sel", kind="hinted")
+    pos_sel = Cpt(EpicsSignal, "-MC10}Pos-Sel", kind='hinted', string=True)
 
 
     def __init__(self, prefix : str, *args, name = "", **kwargs):
@@ -126,50 +140,55 @@ class SlitsWithLookup(Device):
         lst_x = list(self.pos_lookup_x.get())
         lst_y = list(self.pos_lookup_y.get())
         
-        pair_dict = dict((lst_x[k].pair_name, (lst_x[k].pair_val, lst_y[k].pair_val)) for k in range(len(self.defn_x)))
+        filtered_dict = dict((lst_x[k].pair_name, (lst_x[k].pair_val, lst_y[k].pair_val)) for k in range(len(lst_x)) if lst_x[k].pair_name != "Undefined")
         
-        for pair_name in pair_dict:
+        
+        for pair_name in filtered_dict:
             if pair_name == name:
-                return pair_dict[pair_name]     
+                return filtered_dict[pair_name]     
         raise ValueError (f"Could not find {name} in lookup table")
+
     
     
     def get_all_sizes(self):
         lst_x = list(self.pos_lookup_x.get())
         lst_y = list(self.pos_lookup_y.get())
         
-        pair_dict = dict((lst_x[k].pair_name, (lst_x[k].pair_val, lst_y[k].pair_val)) for k in range(len(self.defn_x)))
-        length = len(pair_dict)
+        filtered_dict = dict((lst_x[k].pair_name, (lst_x[k].pair_val, lst_y[k].pair_val)) for k in range(len(lst_x)) if lst_x[k].pair_name != "Undefined")
+        
+
+        length = len(filtered_dict)
+
         print(f"{length} possible sizes:")
         print("----------------------------------")
-        for pair_name in pair_dict:
-            print(f'    {pair_name:_<10} : {pair_dict[pair_name]}')
-
-        print(self.pair_dict)
+        for pair_name in filtered_dict:
+            print(f'    {pair_name:_<10} : {filtered_dict[pair_name]}')
 
 
-    def set_pos(self, pos: str):
+    def set_pos(self, pos: str | tuple):
         lst_x = list(self.pos_lookup_x.get())
         lst_y = list(self.pos_lookup_y.get())
         
-        pair_dict = dict((lst_x[k].pair_name, (lst_x[k].pair_val, lst_y[k].pair_val)) for k in range(len(self.defn_x)))
+        filtered_dict = dict((lst_x[k].pair_name, (lst_x[k].pair_val, lst_y[k].pair_val)) for k in range(len(lst_x)) if lst_x[k].pair_name != "Undefined")
         val = pos
-        if isinstance(val, float):
-            for pair_name in pair_dict:
-                if pair_dict[pair_name] == val:
+        if isinstance(val, tuple):
+            for pair_name in filtered_dict:
+                pair_val = filtered_dict[pair_name]
+                if (float(pos[0]) == float(pair_val[0])) & (float(pos[1]) == float(pair_val[1])):
                     val = pair_name
         return self.pos_sel.set(str(val))
     
 
-    def set(self, size = None):
+    def set(self, size : str | tuple = None):
         if size is None:
-            _xpos = round(self.x.get()[0], 3)
-            _ypos = round(self.y.get()[0], 3)
+            _xpos = float(self.x.read()[(self.name) + '_x_user_setpoint']['value'])
+            _ypos = float(self.y.read()[(self.name) + '_y_user_setpoint']['value'])
 
             lst_x = list(self.pos_lookup_x.get())
             lst_y = list(self.pos_lookup_y.get())
             
-            holes_reverse = dict(((lst_x[k].pair_val, lst_y[k].pair_val), lst_x[k].pair_name) for k in range(len(self.defn_x)))
+            holes_reverse = dict(((lst_x[k].pair_val, lst_y[k].pair_val), lst_x[k].pair_name) for k in range(len(lst_x)) if lst_x[k].pair_name != "Undefined")
+            
             try:
                 _size_slt3_pinhole = holes_reverse[(_xpos, _ypos)]
                 print(f'{_size_slt3_pinhole}um pinhole at {self.name}: {self.x.name} = {_xpos:.4f}, {self.y.name} = {_ypos:.4f}') 
@@ -178,9 +197,21 @@ class SlitsWithLookup(Device):
 
 
         else:
+            if isinstance(size, str):
+                x_pos, y_pos = self.lookup(str(size))
+            else:
+                x_pos, y_pos = size
+
             print('Moving to {} um {}'.format(size, self.name))
-            x_pos, y_pos = self.lookup(str(size))
+            
+            self.set_pos("Undefined")
             x_sts = self.x.set(x_pos)
             y_sts = self.y.set(y_pos)
-            self.set_pos(size)
+            mv_sts = x_sts & y_sts
+            wait(mv_sts)
+
+            _xpos = (self.x.read()[(self.name) + '_x_user_setpoint']['value'])
+            _ypos = (self.y.read()[(self.name) + '_y_user_setpoint']['value'])
+
+            self.set_pos((_xpos, _ypos))
             return x_sts & y_sts
